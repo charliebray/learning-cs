fb95bb5e68f1bd36fbb4bbf0c211e2ddbc67c27f
charliebray <charles.jbray@gmail.com>
2022-07-22 08:49:47 +1000

    Update binary search to remove slicing

--- pythonds3/chapter-6/6.4-binary-search.py
+++ pythonds3/chapter-6/6.4-binary-search.py
@@ -1,4 +1,4 @@
-def binary_search(sorted_list, item):
+def binary_search(sorted_list, item, lower_bound=None, upper_bound=None):
     '''
     sorted_list (list): A sorted list of integers.
     item (int): A integer we are wanting to search for in the sorted list.
@@ -6,18 +6,24 @@ def binary_search(sorted_list, item):
     Returns True if the item exists in the list, otherwise False.
     '''
 
-    # Set our initial bounds, and our initial guess.
-    guess = len(sorted_list) // 2
-
     if len(sorted_list) == 0:
+        return False
+
+    # Set our initial bounds, and our initial guess.
+    if lower_bound == None and upper_bound == None:
+        lower_bound, upper_bound = 0, len(sorted_list) - 1
+
+    guess = (lower_bound + upper_bound) // 2
+
+    if lower_bound == upper_bound and sorted_list[guess] != item:
         return False
 
     while sorted_list[guess] != item:
         if sorted_list[guess] > item:
-            return binary_search(sorted_list[:guess], item)
+            upper_bound = guess
+            return binary_search(sorted_list, item, lower_bound, upper_bound)
         else:
-            return binary_search(sorted_list[guess+1:], item)
+            lower_bound = guess + 1
+            return binary_search(sorted_list, item, lower_bound, upper_bound)
     
     return True
-
-print(binary_search([1, 2, 3, 4, 5], -1))

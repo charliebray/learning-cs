c9188221f6b0a009c5b39f8224227db29eddfbee
charliebray <charles.jbray@gmail.com>
2022-09-14 07:43:52 +1000

    Create a graph object for the adjacency list ADT

--- pythonds3/chapter-8/8.6-adjacency-list.py
+++ pythonds3/chapter-8/8.6-adjacency-list.py
@@ -22,5 +22,30 @@ class Vertex:
     def get_key(self):
         return self.key
 
+class Graph:
 
+    def __init__(self):
+        self.vertices = {}
+
+    def set_vertex(self, key):
+        self.vertices[key] = Vertex(key)
+
+    def get_vertex(self, key):
+        return self.vertices.get(key, None)
+
+    def __contains__(self, key):
+        return key in self.vertices
+
+    def add_edge(self, from_vertex, to_vertex, weight=0):
+        if from_vertex not in self.vertices:
+            self.set_vertex(from_vertex)
+        if to_vertex not in self.vertices:
+            self.set_vertex(to_vertex)
+        self.vertices[from_vertex].set_neighbor(self.vertices[to_vertex], weight)
+
+    def get_vertices(self):
+        return self.vertices.keys()
+
+    def __iter__(self):
+        return iter(self.vertices.values())
     
